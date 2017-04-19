d147fff55b68f2b2788a687d4634aa22faf8862d
vinothkrishna15 <vinothkrishna3@gmail.com>
2017-04-19 16:22:05 +0530

    Commit

--- src/test/java/hello/ApplicationIntegrationTests.java
+++ src/test/java/hello/ApplicationIntegrationTests.java
@@ -2,27 +2,31 @@
  * 
  */
 package hello;
-
-import static org.junit.Assert.assertEquals;
 import static org.springframework.ws.test.client.RequestMatchers.payload;
 import static org.springframework.ws.test.client.ResponseCreators.withPayload;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import javax.xml.transform.Source;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.MockitoAnnotations;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.ws.client.core.WebServiceTemplate;
-import org.springframework.ws.client.support.destination.DestinationProvider;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.test.client.MockWebServiceServer;
 import org.springframework.xml.transform.ResourceSource;
 
+import hello.controller.EmployeeController;
+import hello.wsdl.GetEmployeeRequest;
 import hello.wsdl.GetEmployeeResponse;
 
 /**
@@ -33,27 +37,43 @@ import hello.wsdl.GetEmployeeResponse;
 @ContextConfiguration(classes = { ServiceConfig.class, ServiceClient.class })
 public class ApplicationIntegrationTests {
 
-	@Autowired
-	private ServiceClient client;
-
 	private MockWebServiceServer mockServer;
 	
 	@Mock
-	private WebServiceTemplate wsTemp;
+	private ServiceClient client;
 	
-	private DestinationProvider destinationProvider;
+	@Mock
+	private WebserviceTemplateExtended wsExtend;
+	
+	@Mock
+	private WebServiceGatewaySupport wsGateway;
+	
+	@InjectMocks
+	private EmployeeController empCont;
+	
+	private MockMvc mockMvc;
 
 	@Before
-	public void createServer() throws Exception {
+	public void setup() {
+		 
+        // Process mock annotations
+        MockitoAnnotations.initMocks(this);
+ 
+        // Setup Spring test in standalone mode
+        this.mockMvc = MockMvcBuilders.standaloneSetup(empCont).build();
+ 
+    }
+	/*public void createServer() throws Exception {
 		mockServer = MockWebServiceServer.createServer(client);
 		//Mockito.when(destinationProvider.getDestination()).thenReturn("http://10.144.135.147:8090/ws/employees.wsdl");
-	}
-
+	}*/
 
 	
 	@Test
-	public void testEmployeeService() throws Exception {
-		
+	public void testEmployeeServicee() throws Exception {
+		GetEmployeeRequest request = new GetEmployeeRequest();
+		request.setName("arun");
+		Mockito.when(client.getMarshalSupport(request)).thenReturn("");
 		Source expectedPayload = new ResourceSource(new ClassPathResource("SampleRequest.xml"));
         Source expectedResponse = new ResourceSource(new ClassPathResource("SampleResponse.xml"));
         mockServer.expect(payload(expectedPayload)).andRespond(withPayload(expectedResponse));
@@ -61,6 +81,23 @@ public class ApplicationIntegrationTests {
         //assertThat(getEmpDet, getEmpDet);
         mockServer.verify();
 	}
-
+	@Test
+	public void testEmployeeService() throws Exception {
+	 
+	    
+	    this.mockMvc.perform(post("/getEmployee")
+	            .param("name", "madan"))
+	            .andExpect(status().isOk());
+	 
+	}
+	@Test
+	public void testEmployeeServiceWithError() throws Exception {
+	 
+	    
+	    this.mockMvc.perform(post("/getEmployee")
+	            .param("name", "dd"))
+	            .andExpect(status().isInternalServerError());
+	 
+	}
 	 	
 }
