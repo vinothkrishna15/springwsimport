655374fdc76fb7c4f21932eecda9cac082a57935
vinothkrishna15 <vinothkrishna3@gmail.com>
2017-04-19 15:07:04 +0530

    Junit modified.

--- src/test/java/hello/ApplicationIntegrationTests.java
+++ src/test/java/hello/ApplicationIntegrationTests.java
@@ -3,27 +3,27 @@
  */
 package hello;
 
+import static org.junit.Assert.assertEquals;
 import static org.springframework.ws.test.client.RequestMatchers.payload;
 import static org.springframework.ws.test.client.ResponseCreators.withPayload;
-import hello.controller.EmployeeController;
-import hello.wsdl.Employee;
-import hello.wsdl.GetEmployeeResponse;
 
 import javax.xml.transform.Source;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.client.support.destination.DestinationProvider;
 import org.springframework.ws.test.client.MockWebServiceServer;
-import org.springframework.xml.transform.StringSource;
+import org.springframework.xml.transform.ResourceSource;
+
+import hello.wsdl.GetEmployeeResponse;
 
 /**
  * @author TCS
@@ -33,75 +33,34 @@ import org.springframework.xml.transform.StringSource;
 @ContextConfiguration(classes = { ServiceConfig.class, ServiceClient.class })
 public class ApplicationIntegrationTests {
 
-	@Configuration
-	static class ApplicationIntegrationTestsConfiguration {
-
-		@Bean
-		public ServiceClient client() {
-			return Mockito.mock(ServiceClient.class);
-		}
-
-		@Bean
-		public EmployeeController employeeController() {
-			return new EmployeeController();
-		}
-	}
-
 	@Autowired
 	private ServiceClient client;
 
-	@Autowired
-	private EmployeeController empCont;
-
 	private MockWebServiceServer mockServer;
+	
+	@Mock
+	private WebServiceTemplate wsTemp;
+	
+	private DestinationProvider destinationProvider;
 
 	@Before
 	public void createServer() throws Exception {
 		mockServer = MockWebServiceServer.createServer(client);
-
+		//Mockito.when(destinationProvider.getDestination()).thenReturn("http://10.144.135.147:8090/ws/employees.wsdl");
 	}
 
-	public void setup() throws Exception {
-		Employee emp = new Employee();
-		emp.setId(21);
-		emp.setDesignation("ITA");
-		emp.setName("arun");
-		//emp.setRole();
 
-		  //Mockito.when(this.empCont.empDetails(("arun")).thenReturn(emp);
-		  }
-
-	@SuppressWarnings("unused")
+	
 	@Test
-	public void customerClient() throws Exception {
-
-		empCont.empDetails("arun");
-		Source requestPayload = new StringSource(
-				" xmlns:gs='http://com.example/webservice/generatedwsdl'>"
-						+ "<soapenv:Header/> <soapenv:Body> "
-						+ "<gs:getEmployeeRequest>"
-						+ " <gs:name>arun</gs:name>"
-						+ "      </gs:getEmployeeRequest>");
-		Source responsePayload = new StringSource(
-				"<ns2:getEmployeeResponse xmlns:ns2='http://com.example/webservice/generatedwsdl'>"
-						+ "  <ns2:employee>"
-						+ " <ns2:name>arun</ns2:name>"
-						+ " <ns2:id>535141</ns2:id>"
-						+ " <ns2:designation>ITA</ns2:designation>"
-						+ " <ns2:role>developer</ns2:role>    </ns2:employee> </ns2:getEmployeeResponse>");
-
-		mockServer.expect(payload(requestPayload)).andRespond(
-				withPayload(responsePayload));
-		GetEmployeeResponse result = client.getEmployeeDetails("arun");
-		// assertEquals(10, result);
-		mockServer.verify();
+	public void testEmployeeService() throws Exception {
+		
+		Source expectedPayload = new ResourceSource(new ClassPathResource("SampleRequest.xml"));
+        Source expectedResponse = new ResourceSource(new ClassPathResource("SampleResponse.xml"));
+        mockServer.expect(payload(expectedPayload)).andRespond(withPayload(expectedResponse));
+        GetEmployeeResponse getEmpDet = client.getEmployeeDetails("arun");
+        //assertThat(getEmpDet, getEmpDet);
+        mockServer.verify();
 	}
 
-	@Test
-	public void testGetEmployeeDetails() throws Exception {
-		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(this.empCont).build();
-		//mockMvc.perform(
-			//	post("/getEmployee").param("arun"));
-				
-	}
+	 	
 }
